c6f410c6532860b8357f46cbcda9241297b37e56
Orbitus <orbitus@orbitusrobotics.com>
2016-06-08 10:04:13 -0400

    Added complete project

---
+++ USBStick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  USBStick
+//
+//  Created by Rudy Aramayo on 6/8/16.
+//  Copyright © 2016 Verizon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ USBStick/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  USBStick
+//
+//  Created by Rudy Aramayo on 6/8/16.
+//  Copyright © 2016 Verizon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
